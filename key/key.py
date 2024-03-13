d46c86d4011fd18de26003472056a42b4e7a6390
kazu <kzs321kzss@gmail.com>
2024-03-13 17:52:40 +0900

    pid unko

--- key/key.py
+++ key/key.py
@@ -3,28 +3,40 @@ import sys
 import rclpy
 from rclpy.node import Node 
 from std_msgs.msg import String
-dir={"w":0,"a":-90,"s":180,"d":90}
+dir={"w":0,"a":-90,"s":180,"d":90,"Key.up":[0,1],"Key.down":[0,-1],"Key.right":[1,0],"Key.left":[-1,0]}
+old=["",""]
 
 class pineapple(Node):
     def __init__(self):
         super().__init__("key")
-        self.pub=self.create_publisher(String,"to_pc_cmd",0)
-        self.pub_n=self.create_publisher(String,"to_nucleo_cmd",0)
-        self.timer = self.create_timer(0.01,self.call)
+        self.pub=self.create_publisher(String,"cmd",10)
+        self.pub_n=self.create_publisher(String,"nucleo_cmd",10)
+        self.timer = self.create_timer(0.1,self.call)
         self.cmd=String()
         self.keys=set()
 
     def call(self):
         d=0
         i=0
+        c=[0,0]
         for key in self.keys:
-            if key in ["w","a","s","d"]:
+            if key in list("wasd"):
                 d+=dir[key]
                 i+=1
+            elif key[4:] in ["up","down","right","left"]:
+                c[0]+=dir[key][0]
+                c[1]+=dir[key][1]
         if i!=0:
             d//=i
+        self.cmd.data="#C"+str(c[0]).replace("-1","-").replace("1","+")+str(c[1]).replace("-1","-").replace("1","+")
+        if old[0]!=self.cmd.data:
+            self.pub.publish(self.cmd)
+            old[0]=self.cmd.data
         self.cmd.data="/cd "+str(d)
-        self.pub.publish(self.cmd)
+        if old[1]!=self.cmd.data:
+            self.pub.publish(self.cmd)
+            old[1]=self.cmd.data
+
     
     def on_press(self,key):
         try:
@@ -32,17 +44,22 @@ class pineapple(Node):
             self.keys.add(key.char)
         except AttributeError:
             print('spkey press: {}'.format(key))
+            self.keys.add(str(key))
     
     def on_release(self,key):
         try:
             print('release: {}'.format(key.char))
             self.keys.remove(key.char)
-            if key.char=="r":
+            if key.char in list("rpceuz"):
                 r=String()
-                r.data="/r"
+                r.data="?"+str(key.char)
                 self.pub_n.publish(r)
         except AttributeError:
             print('release: {}'.format(key))
+            try:
+                self.keys.remove(str(key))
+            except:
+                pass
         except KeyError:
             pass
         if( key == keyboard.Key.esc):
